e53a9129551a8491d87fdf51806ac9e08266751e
JosueEspinoS <josue.espino.soto.git@gmail.com>
2022-04-20 21:18:16 -0500

    PAso 7: Agregando requerimeinto 2 (Modificacion y respectiva prueba)

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,10 +1,16 @@
 
 const User = require('./../../app/models/User')
 describe("Prueba de jest", () => {
-    test("Creacion de user", () => {
+    test("Requerimiento 1: Creacion de user", () => {
         const user = new User(1,"JosueEspino","Josue","Bio", "dateCreated", "lastUpdate")
         expect(user.id).toBe(1)
         expect(user.username).toBe("JosueEspino")
 
+    })
+
+    test("Requerimiento 2: Fechas en atributos de User",() => {
+        const user = new User(1,"JosueEspino", "Josue" , "Bio")
+        expect(user.dateCreated).not.toBeUndefined()
+        expect(user.lastUpdate.not.toBeUndefined()
     })
 })
