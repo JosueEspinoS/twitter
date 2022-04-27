ce6e51a74a7594d2b092ffa15ced587d5d55a621
JosueEspinoS <josue.espino.soto.git@gmail.com>
2022-04-26 19:11:58 -0500

    Agregando UserServices

---
+++ app/services/UserService.js
@@ -0,0 +1,23 @@
+const User = require('./../models/User')
+
+class UserService {
+    static create(id, username, name){
+        return new User(id, username, name, "Sin Bio")
+    }
+
+    static getInfo(user){        
+        return [user.id, user.username, user.name, user.bio]
+    }
+
+    static updateUserUsername(user, newUsername){
+        return user.username = newUsername
+    }
+
+    static getAllUsernames([user1, user2, user3]){
+        return [user1.username, user2.username, user3.username]
+    }    
+    
+    
+}
+
+module.exports = UserService
